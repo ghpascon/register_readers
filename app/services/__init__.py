f0da3da8e6b0369742e83ec64b3d5a2ab7c27cc7
Gabriel Henrique Pascon <gh.pascon@gmail.com>
2026-04-01 15:28:58 -0300

    support mysql gcp
    Bump version to v8.2.2

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,7 +1,7 @@
 from .rfid._main import RfidManager
 from app.core import DEVICES_PATH, EXAMPLE_PATH
 
-rfid_manager = RfidManager(devices_path=DEVICES_PATH, example_path=EXAMPLE_PATH)
 from .controller import Controller
 
+rfid_manager = RfidManager(devices_path=DEVICES_PATH, example_path=EXAMPLE_PATH)
 controller_manager = Controller()
